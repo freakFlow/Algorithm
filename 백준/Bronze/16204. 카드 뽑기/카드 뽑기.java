6bf17df5f5c03e4693f0ff845148fdc8bca052bd
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-03-03 21:02:01 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static int N, M, K;
+	static int ans;
+	
+	public static void main(String[] args) throws IOException {
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		ans = Math.min(M, K) + Math.min(N-M, N-K);
+		
+		sb.append(ans);
+		
+		System.out.println(sb);
+	}
+}
