0619f233e8e67edb46b299c6bc735c0d8ffd77eb
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-03-15 21:31:09 +0900

    [Bronze IV] Title: Rook, Time: 12 ms, Memory: 70348 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cob
@@ -0,0 +1,22 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Main.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       Begin.
+           DISPLAY '  ___  ___  ___'
+           DISPLAY '  | |__| |__| |'
+           DISPLAY '  |           |'
+           DISPLAY '   \_________/'      
+           DISPLAY '    \_______/'      
+           DISPLAY '     |     |'   
+           DISPLAY '     |     |'
+           DISPLAY '     |     |'
+           DISPLAY '     |     |'      
+           DISPLAY '     |_____|' 
+           DISPLAY '  __/       \__'      
+           DISPLAY ' /             \'      
+           DISPLAY '/_______________\'      
+           STOP RUN.
