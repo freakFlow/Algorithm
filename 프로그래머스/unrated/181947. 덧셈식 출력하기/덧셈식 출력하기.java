9dc1bde7ed48259fae0d580f4b09b4b49e93ace0
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-10-12 08:49:37 +0900

    [level unrated] Title: 덧셈식 출력하기, Time: 141.34 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    
+    static int a, b;
+    
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        
+        sb.append(a).append(" + ").append(b).append(" = ").append(a + b);
+        
+        System.out.print(sb);
+    }
+}
