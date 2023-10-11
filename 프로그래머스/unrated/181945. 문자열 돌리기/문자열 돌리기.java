ac9b8ce63ecd29e9fe1687d465c228852b27238a
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-10-12 08:54:31 +0900

    [level unrated] Title: 문자열 돌리기, Time: 125.89 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    
+    static String str;
+    
+    public static void main(String[] args) throws IOException {
+        str = br.readLine();
+        
+        for(int i=0; i<str.length(); i++){
+            sb.append(str.charAt(i)).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+}
