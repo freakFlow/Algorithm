12fb8927154682c9132c87ca2934229e532ff53f
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-11-05 16:17:34 +0900

    [level unrated] Title: 무작위로 K개의 수 뽑기, Time: 223.68 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(!list.contains(arr[i])){
+                list.add(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[k];
+        for(int i=0; i<k; i++){
+            if(i < list.size()){
+                answer[i] = list.get(i);
+                continue;
+            }
+            
+            answer[i] = -1;
+        }
+        
+        return answer;
+    }
+}
