32ecc3f45fee99bd714658afd3d6c6f2da2b8a83
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-12-08 15:40:35 +0900

    [level unrated] Title: 평행, Time: 0.06 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120875. 평행/평행.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] dots) {
+        
+        for(int i=0; i<4; i++){
+            for(int j=i + 1; j<4; j++){
+                double angle1 = (double)(dots[j][1] - dots[i][1]) / (dots[j][0] - dots[i][0]);
+                int x = -1, y = -1;
+                for(int k=0; k<4; k++){
+                    if(k == i || k == j) continue;
+                    if(x == -1){
+                        x = dots[k][0];
+                        y = dots[k][1];
+                    }else{
+                        x -= dots[k][0];
+                        y -= dots[k][1];
+                    }
+                }
+                
+                if(Math.abs(angle1 - (double)y / x) < Math.pow(10, -15)) return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
