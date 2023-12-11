0fecec200c5a5db0817c169a36fa39b982b8adb3
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-12-11 16:32:48 +0900

    [Silver V] Title: 수들의 합 5, Time: 180 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,40 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException {
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int ans = 1;// N 포함
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-		for(int div=2; div<=N; div++) { // div: 연속된 자연수의 개수
-			int res = N / div; // res: 연속된 자연수 중 중간에 해당하는 숫자
-			int remain = N % div; 
-			int startNum = res - div / 2; // 연속된 자연수의 맨 처음 숫자
-			if(div % 2 == 0) {
-				startNum++; // 연속된 자연수의 개수가 짝수일 경우, res가 정확히 중간의 숫자가 아니므로 1을 더함
-			}
-			if(startNum < 1) {
-				break; // 연속된 자연수의 맨 처음 숫자가 1보다 작으면 이후 탐색은 무의미
-			}
-			if(div % 2 == 0 && remain == div / 2) {// div가 짝수이면 나머지가 div/2만큼 있어야 등차 수열 완성 -> res-1, res, res+1, res+2 ...
-				ans++;
-			}
-			if(div % 2 == 1 && remain == 0) {// div가 홀수이면 나머지가 없어야 등차 수열 완성 -> res-1, res, res+1 ...
-				ans++;
-			}
-		}
-		
-		sb.append(ans);
-		
-		System.out.println(sb);
-	}
-	
+    static int N;
+    static int sum;
+    static int startIdx, endIdx;
+    static int answer;
+
+    public static void main(String[] args) throws IOException{
+        N = Integer.parseInt(br.readLine());
+        sum = 1;
+        startIdx = endIdx = 1;
+        answer = 1;
+
+        while(endIdx < N){
+            if(sum == N){
+                answer++;
+                sum += ++endIdx;
+            }else if(sum < N){
+                sum += ++endIdx;
+            }else{
+                sum -= startIdx++;
+            }
+        }
+
+        System.out.print(answer);
+    }
 }
