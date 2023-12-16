80bbe90578f22ed880676eba9b11eb5d8caf5f79
freakFlow <114668941+freakFlow@users.noreply.github.com>
2023-12-16 21:52:01 +0900

    [Silver V] Title: K번째 수, Time: 2092 ms, Memory: 376348 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, K;
+    static int[] num;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        num = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        answer = num[K - 1];
+
+        System.out.println(answer);
+    }
+
+}
