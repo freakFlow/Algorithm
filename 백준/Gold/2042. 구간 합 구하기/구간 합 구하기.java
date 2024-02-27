7a80601325c1c332852a7751a1da6b6cb47b5a5b
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-02-27 17:38:53 +0900

    [Gold I] Title: 구간 합 구하기, Time: 548 ms, Memory: 96364 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -2,57 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	
-	static int N, M, K;
-	static int a, b;
-	static long c;
-	static long[] fwt;
-	
-	public static void main(String[] args) throws IOException {
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		fwt = new long[N + 1];
-		for(int i=1; i<fwt.length; i++) {
-			update(i, Long.parseLong(br.readLine()));
-		}
-		
-		for(int i=0; i<M+K; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			c = Long.parseLong(st.nextToken());
-			
-			if(a == 1) {
-				update(b, c - (sum(b) - sum(b-1)));
-			}else {
-				sb.append(sum((int)c) - sum(b-1)).append('\n');
-			}
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static long sum(int idx) {
-		long sum = 0;
-		while(idx > 0) {
-			sum += fwt[idx];
-			idx &= idx-1;
-		}
-		
-		return sum;
-	}
-	
-	static void update(int idx, long val) {
-		while(idx<fwt.length) {
-			fwt[idx] += val;
-			idx += idx & -idx;
-		}
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M, K;
+    static long a, b, c;
+    static int index;
+    static long[] segmentTree;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        index = (int)Math.pow(2, (int)Math.ceil(Math.log(N) / Math.log(2)));
+        segmentTree = new long[index * 2];
+
+        for(int n=0; n<N; n++){
+            segmentTree[index + n] = Long.parseLong(br.readLine());
+        }
+
+        for(int i=index-1; i>0; i--){
+            segmentTree[i] = segmentTree[2 * i] + segmentTree[2 * i + 1];
+        }
+
+        for(int i=0; i<M + K; i++){
+            st = new StringTokenizer(br.readLine());
+            a = Long.parseLong(st.nextToken());
+            b = Long.parseLong(st.nextToken());
+            c = Long.parseLong(st.nextToken());
+
+            if(a == 1) update();
+            else subTotal();
+        }
+
+        System.out.print(sb);
+    }
+
+    static void subTotal(){
+        int startIdx = (int)b + index - 1;
+        int endIdx = (int)c + index - 1;
+        long sum = 0;
+
+        while(startIdx <= endIdx){
+            if(startIdx % 2 == 1) sum += segmentTree[startIdx];
+            if(endIdx % 2 == 0) sum += segmentTree[endIdx];
+            startIdx = (startIdx + 1) / 2;
+            endIdx = (endIdx - 1) / 2;
+        }
+
+        sb.append(sum).append('\n');
+    }
+
+    static void update(){
+        int idx = (int)b + index - 1;
+        long diff = segmentTree[idx] - c;
+
+        while(idx > 0){
+            segmentTree[idx] -= diff;
+            idx /= 2;
+        }
+    }
 }
