141adfe88c4c6a985a06906121dcd9e541303048
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-11-23 21:57:20 +0900

    [Gold V] Title: 인내의 도미노 장인 호석, Time: 0 ms, Memory: 2076 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dr[] = {0, 1, 0, -1};
+int dc[] = {1, 0, -1, 0};
+
+int N, M, R;
+int arr[101][101];
+bool toppled[101][101];
+queue<pair<int, int>> q;
+int ans;
+
+int toDir(char dir){
+    switch(dir){
+        case 'E':
+            return 0;
+        case 'S':
+            return 1;
+        case 'W':
+            return 2;
+        case 'N':
+            return 3;
+    }
+    return 0;
+}
+
+bool isIn(int r, int c){
+    return r>0 && r<=N && c>0 && c<=M;
+}
+
+int main() {
+    cin >> N >> M >> R;
+
+    for(int n=1; n<=N; n++){
+        for(int m=1; m<=M; m++){
+            cin >> arr[n][m];
+        }
+    }
+
+    for(int r=0; r<R; r++){
+        int score = 0;
+        int sr, sc, d;
+        char dir;
+        
+        cin >> sr >> sc >> dir;
+        d = toDir(dir);
+
+        if(!toppled[sr][sc]){
+            q.push({sr, sc});
+
+            while(!q.empty()){
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+
+                toppled[r][c] = true;
+                score++;
+
+                for(int k=1; k<arr[r][c]; k++){
+                    int nr = r + dr[d] * k;
+                    int nc = c + dc[d] * k;
+
+                    if(isIn(nr, nc) && !toppled[nr][nc]) q.push({nr, nc});
+                }
+            }
+        }
+
+        ans += score;
+
+        cin >> sr >> sc;
+
+        toppled[sr][sc] = false;
+    }
+
+    cout << ans << endl;
+    for(int n=1; n<=N; n++){
+        for(int m=1; m<=M; m++){
+            cout << (toppled[n][m] ? "F " : "S ");
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
