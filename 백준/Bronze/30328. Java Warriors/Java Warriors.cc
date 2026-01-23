e526e1d6fbc708734620e609a3e36a4f5e2dfba8
freakFlow <114668941+freakFlow@users.noreply.github.com>
2026-01-23 20:36:14 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int n;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	cout << n * 4000;
+	
+	return 0;
+}
